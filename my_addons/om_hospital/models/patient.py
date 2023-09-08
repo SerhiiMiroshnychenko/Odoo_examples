36d001a28d8c725b64afaf010a8d6c063f5ad9d1
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-09-08 17:37:24 +0300

    #main [DEV] Added compute field "uppercase_name" to "hospital.patient" in "om_hospital" module.

--- my_addons/om_hospital/models/patient.py
+++ my_addons/om_hospital/models/patient.py
@@ -17,6 +17,16 @@ class HospitalPatient(models.Model):
             ('female', 'Female'),
         ],
     )
+    uppercase_name = fields.Char(compute='_compute_uppercase_name', store=True)
+
+    @api.depends('name')
+    def _compute_uppercase_name(self):
+        """
+        Compute and set the 'uppercase_name' field value by uppercase the 'name' field.
+        """
+        for patient in self:
+            if patient.name:
+                patient.uppercase_name = patient.name.upper()
 
     @api.onchange('age')
     def _onchange_age(self):
