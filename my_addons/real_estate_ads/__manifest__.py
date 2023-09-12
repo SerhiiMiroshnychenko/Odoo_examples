9163d1992229c8f0108a5ef637edd01112e99878
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-09-12 17:48:32 +0300

    #main [ADD] Added the "real_estate_ads" module.

---
+++ my_addons/real_estate_ads/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Real Estate Ads",
+    "version": "16.0.1.0.0",
+    "website": "https://github.com/SerhiiMiroshnychenko/Odoo_examples",
+    "author": "Serhii Miroshnychenko",
+    "summary": "Real estate module to show available properties",
+    "sequence": -101,
+    "description": """
+Real estate module to show available properties
+    """,
+    "category": "Sales",
+    "depends": [],
+    "data": [],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3"
+}
