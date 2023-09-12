fa10419a4150725ad04e8cbc39e67f6a76d5909a
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-09-12 16:43:16 +0300

    #main [IMP] Improved the manifest file in the "om_hospital" module

--- my_addons/om_hospital/__manifest__.py
+++ my_addons/om_hospital/__manifest__.py
@@ -1,8 +1,17 @@
+# -*- coding: utf-8 -*-
 {
     'name': 'Hospital Management System',
-    'author': 'Serhii Miroshnychenko',
-    'website': 'https://github.com/SerhiiMiroshnychenko',
+    'version': '16.0.1.0.0',
     'summary': 'Odoo 16 Hospital Management System',
+    'sequence': -100,
+    'description': """
+Clinic Administration Software
+==============================
+An Odoo module for automation of hospital system management
+""",
+    'category': 'Uncategorized',
+    'author': 'Serhii Miroshnychenko',
+    'website': 'https://github.com/SerhiiMiroshnychenko/Odoo_examples/tree/main/my_addons/om_hospital',
     'depends': ['mail', 'estate'],
     'data': [
         'security/ir.model.access.csv',
@@ -10,5 +19,8 @@
         'views/patient.xml',
         'views/doctor.xml',
         'views/menu.xml',
-    ]
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
 }
