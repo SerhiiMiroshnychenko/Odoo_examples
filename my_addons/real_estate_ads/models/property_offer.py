e8d179e6dbd393be5c2a4119ff5a13b68cf01c54
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-09-15 10:39:37 +0300

    #main [DEV] Add "autovacuum" decorator to "real.property.offer" model in 'real_estate_abs' module.

--- my_addons/real_estate_ads/models/property_offer.py
+++ my_addons/real_estate_ads/models/property_offer.py
@@ -38,3 +38,15 @@ class PropertyOffer(models.Model):
     def _inverse_deadline(self):
         for offer in self:
             offer.validity = (offer.deadline - offer.creation_date).days
+
+    @api.autovacuum
+    def _clean_offers(self):
+        """
+        Decorate a method so that it is called by the daily vacuum cron job (model ir.autovacuum).
+        This is typically used for garbage-collection-like tasks that do not deserve a specific cron job.
+
+        Прикрасьте метод так, щоб він викликався щоденним завданням вакуумної синхронізації (модель ir.autovacuum).
+        Це зазвичай використовується для завдань, подібних до збирання сміття,
+        які не заслуговують на певне завдання cron.
+        """
+        self.search([('status', '=', 'refused')]).unlink()
