7771acbb4415e87ab28a5f44175b8087bde3997c
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-09-19 17:17:52 +0300

    #main [DEV] Added new custom tracer (logging) to 'real_estate_abs' module.

---
+++ my_addons/real_estate_ads/models/log_trace.py
@@ -0,0 +1,27 @@
+import logging
+import colorlog
+
+TRACE = 23
+logging.addLevelName(TRACE, 'TRACE')
+
+
+def trace(self, message, *args, **kwargs):
+    if self.isEnabledFor(TRACE):
+        self._log(TRACE, message, args, **kwargs)
+
+
+logging.Logger.trace = trace
+formatter = colorlog.ColoredFormatter(
+    fmt='%(message_log_color)s%(message)s%(reset)s',
+    secondary_log_colors={
+        'message': {
+            'TRACE': 'red'
+        }
+    }
+)
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+tracer = logging.getLogger(__name__)
+tracer.addHandler(handler)
+
+_trace = tracer.trace
