9251b5a9aa90f5a97a7fafaaec19cfea827a4e02
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-10-31 11:46:05 +0200

    main [ADD] real_estate_ads_sales_person

---
+++ my_addons/real_estate_ads_sales_person/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    'name': 'Real Estate Ads - Sales Person',
+    'version': '16.0.0.0.0',
+    'summary': 'Real Estate Ads - Sales Person',
+    'description': '''
+        Show real estate properties linked to a sales person
+     ''',
+    'category': 'Sales',
+    
+    'author': 'Serhii Miroshnychenko',
+    'website': 'https://github.com/SerhiiMiroshnychenko/Odoo_examples',
+    'license': 'LGPL-3',
+    
+    'depends': [
+        'real_estate_ads',
+        'base'
+    ],
+    #'data': [
+    #    ''
+    #],
+    # 'demo': [
+    #     'demo/document_page.xml'
+    # ],
+    #'assets': {
+    #    '':[
+    #        ''
+    #    ]
+    #}
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
