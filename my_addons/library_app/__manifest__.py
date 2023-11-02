0e3e489f9240d019bc408aa7c63296855a2a32b8
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-11-02 18:51:10 +0200

    main [DEV] library_app: Changed `__manifest__.py`

--- my_addons/library_app/__manifest__.py
+++ my_addons/library_app/__manifest__.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "library_app",
+    'name': "Library Management",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': "Manage library catalog and book lending.",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Serhii Miroshnychenko",
+    "license": "AGPL-3",
+    'website': "https://github.com/SerhiiMiroshnychenko/Odoo_examples/tree/main/my_addons/library_app",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Services/Library',
+    'version': '16.0.1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -32,4 +31,6 @@
     'demo': [
         'demo/demo.xml',
     ],
+
+    'application': True,
 }
