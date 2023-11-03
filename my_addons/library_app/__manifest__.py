27888f8b943193cd4afb57001a4793621d7a4485
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-11-03 12:58:10 +0200

    main [DEV] library_app: Added menu items.

--- my_addons/library_app/__manifest__.py
+++ my_addons/library_app/__manifest__.py
@@ -23,6 +23,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        "views/library_menu.xml",
     ],
     # only loaded in demonstration mode
     'demo': [
