f1ba85294eaafd2a684220284c020baa9142522a
Serhii Miroshnychenko <veprzlisu@gmail.com>
2023-11-05 18:26:46 +0200

    main [DEV] library_app: Added book_list_template.xml

--- my_addons/library_app/__manifest__.py
+++ my_addons/library_app/__manifest__.py
@@ -1,34 +1,35 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Library Management",
+    "name": "Library Management",
 
-    'summary': "Manage library catalog and book lending.",
+    "summary": "Manage library catalog and book lending.",
     "sequence": -100,
 
-    'author': "Serhii Miroshnychenko",
+    "author": "Serhii Miroshnychenko",
     "license": "AGPL-3",
-    'website': "https://github.com/SerhiiMiroshnychenko/Odoo_examples/tree/main/my_addons/library_app",
+    "website": "https://github.com/SerhiiMiroshnychenko/Odoo_examples/tree/main/my_addons/library_app",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Services/Library',
-    'version': '16.0.1.0.0',
+    "category": "Services/Library",
+    "version": "16.0.1.0.0",
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    "depends": ["base"],
 
     # always loaded
-    'data': [
+    "data": [
         "security/library_security.xml",
-        'security/ir.model.access.csv',
-        'views/book_view.xml',
+        "security/ir.model.access.csv",
+        "views/book_view.xml",
         "views/library_menu.xml",
+        "views/book_list_template.xml",
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    "demo": [
+        "demo/demo.xml",
     ],
 
-    'application': True,
+    "application": True,
 }
