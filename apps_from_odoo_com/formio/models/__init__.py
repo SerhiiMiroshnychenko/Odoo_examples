df0f205dda31bac72e72c91bb9310bf7b7c5b2e2
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-02-10 10:12:28 +0200

    Update

---
+++ apps_from_odoo_com/formio/models/__init__.py
@@ -0,0 +1,22 @@
+# Copyright Nova Code (http://www.novacode.nl)
+# See LICENSE file for full licensing details.
+
+from . import formio_builder
+from . import formio_builder_js_options
+from . import formio_builder_translation
+from . import formio_default_asset_css
+from . import formio_extra_asset
+from . import formio_form
+from . import formio_res_model
+from . import formio_version
+from . import formio_version_asset
+from . import formio_version_translation
+from . import formio_version_github_tag
+from . import formio_translation_source
+from . import formio_translation
+from . import ir_actions
+from . import ir_attachment
+from . import ir_http
+from . import ir_view
+from . import res_config_settings
+from . import res_lang
