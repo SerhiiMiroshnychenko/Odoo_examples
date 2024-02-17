4c5567610013a269a43b6a56d88b03134afd3511
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-02-17 09:24:06 +0200

    Update

---
+++ apps_from_odoo_com/web_listview_sticky_header/__manifest__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "List View Sticky Header",
+
+    'summary': """
+      List View Sticky Header v16.0""",
+
+    'description': """
+	- Sticky Header
+	- List Sticky Header
+	- Header Sticky
+	- Fix Header
+	- Header Fix
+	- Table Header Fix
+	- Table List Fix
+	- Table Header Sticky
+	Tree view header,
+	header freeze,
+	odoo custom view app,
+	sticky view in odoo,
+	odoo view app,
+	listviewstickyheader,
+	all in one sticky,
+	freeze view in odoo,
+	freeze list view ,
+	sticky pivot view,
+	sticky pivot view header,
+	fixed pivot view header,
+	freeze header,
+	pivot view freeze header,
+    """,
+
+    'author': "Ksolves India Ltd.",
+    'website': "https://store.ksolves.com/",
+    'license': 'LGPL-3',
+    'currency': 'EUR',
+    'category': 'Tools',
+    'version': '16.0.1.0.2',
+    'images': ['static/description/List_View_Sticky_Header.jpg'],
+    'depends': ['base'],
+
+    'assets': {
+        'web.assets_backend': [
+            'web_listview_sticky_header/static/scss/Listview_sticky_header.scss',
+        ]
+    }
+
+}
