f37ab27523a9933d8d12245587d077e07ced3177
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-05-10 09:01:13 +0300

    Update

--- my_addons/fish/controllers/fish_controllers.py
+++ my_addons/fish/controllers/fish_controllers.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+import logging
+
 from odoo import http
 from odoo.http import request
+
+logger = logging.getLogger(__name__)
 
 
 class FishHome(http.Controller):
@@ -14,4 +18,6 @@ class FishHome(http.Controller):
 
     @http.route('/fish/<model("fish.fish"):fish>/', type='http', auth='user', website=True)
     def fish(self, fish):
+        print(f"\n\n{fish = }\n\n")
+        logger.info(f"\n\n{fish = }\n")
         return http.request.render('fish.remarks', {'fish': fish})
