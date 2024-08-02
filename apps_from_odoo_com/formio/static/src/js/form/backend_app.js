1cdc41fc43e71f0385d9c10752fd814dfd11ee02
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-08-02 09:09:56 +0300

    Update

--- apps_from_odoo_com/formio/static/src/js/form/backend_app.js
+++ apps_from_odoo_com/formio/static/src/js/form/backend_app.js
@@ -95,6 +95,19 @@ function app() {
                 }, 500);
             }
         }
+
+        scrollParent() {
+            if (this.params.hasOwnProperty('scroll_into_view_selector')
+                && this.params.scroll_into_view_selector) {
+                const params = {
+                    scroll_into_view_selector: this.params.scroll_into_view_selector
+                };
+                window.parent.postMessage({odooFormioMessage: 'formioScrollIntoView', params: params});
+            }
+            else {
+                window.parent.postMessage({odooFormioMessage: 'formioScrollTop', params: {}});
+            }
+        }
     }
 
     const app = new App();
